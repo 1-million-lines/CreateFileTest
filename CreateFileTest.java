9ec941faaace124203ac3101bca65a18f53d0625
1-million-lines <55835548+1-million-lines@users.noreply.github.com>
2019-09-26 16:28:29 +0300

    Create CreateFileTest.java

---
+++ CreateFileTest.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class CreateFileTest {
+	public static void main(final String... args) {
+		try {
+			new File("output.txt").createNewFile();
+			new File(File.separator + "output.txt").createNewFile();
+			new File("docs").mkdir();
+			new File(File.separator + "docs").mkdir();
+
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+}
